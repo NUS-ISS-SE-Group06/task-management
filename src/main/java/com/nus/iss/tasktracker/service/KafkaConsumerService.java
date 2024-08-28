1821c7361beee4689d58d2c022850b501cc2a668
Onggo Wahyudi <carnegzs@gmail.com>
2024-08-28 22:36:05 +0800

    TTP2-9 Task Management Docker Kubernetes Deployment script & Configuration

--- src/main/java/com/nus/iss/tasktracker/service/KafkaConsumerService.java
+++ src/main/java/com/nus/iss/tasktracker/service/KafkaConsumerService.java
@@ -1,5 +1,11 @@
 package com.nus.iss.tasktracker.service;
 
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+
 public interface KafkaConsumerService {
-    void listenToTaskProcessingEvents(String message);
+    void listenToTaskProcessingEvents(ConsumerRecord<String, String> record);
+    void handleUserInfoCreated(String message);
+    void handleUserInfoDeleted(String message);
+    void handleGroupInfoCreated(String message);
+    void handleGroupInfoDeleted(String message);
 }
--- src/main/java/com/nus/iss/tasktracker/service/impl/KafkaConsumerServiceImpl.java
+++ src/main/java/com/nus/iss/tasktracker/service/impl/KafkaConsumerServiceImpl.java
@@ -3,14 +3,50 @@ package com.nus.iss.tasktracker.service.impl;
 import com.nus.iss.tasktracker.service.KafkaConsumerService;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 
 @Service
 public class KafkaConsumerServiceImpl implements KafkaConsumerService {
 
     @Override
     @KafkaListener(topics = {"UserInfoCrated","UserInfoDeleted", "GroupInfoCreated", "GroupInfoDeleted", "TaskInfoDeleted", "TaskInfoCreated"}, groupId = "task-processing-group")
-    public void listenToTaskProcessingEvents(String message) {
+    public void listenToTaskProcessingEvents( ConsumerRecord<String, String> record) {
+        String topic = record.topic();
+        String message = record.value();
+        System.out.println("topic]: " + topic);
         System.out.println("Receive messages: " + message);
 
+        switch (topic) {
+            case "UserInfoCreated":
+                handleUserInfoCreated(message);
+                break;
+            case "UserInfoDeleted":
+                handleUserInfoDeleted(message);
+                break;
+            case "GroupInfoCreated":
+                handleGroupInfoCreated(message);
+                break;
+            case "GroupInfoDeleted":
+                handleGroupInfoDeleted(message);
+                break;
+        }
+    }
+
+    @Override
+    public void handleUserInfoCreated(String message) {
+        System.out.println("Handling UserInfoCreated: " + message);
+    }
+    @Override
+    public void handleUserInfoDeleted(String message) {
+        System.out.println("Handling UserInfoDeleted: " + message);
+    }
+
+    @Override
+    public void handleGroupInfoCreated(String message) {
+        System.out.println("Handling GroupInfoCreated: " + message);
+    }
+    @Override
+    public void handleGroupInfoDeleted(String message) {
+        System.out.println("Handling GroupInfoDeleted: " + message);
     }
 }
