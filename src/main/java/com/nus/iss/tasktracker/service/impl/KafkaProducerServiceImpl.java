82f34d96f621493ff621b352fd91e8faf2d9534a
sivabalaji1986 <42265473+sivabalaji1986@users.noreply.github.com>
2024-10-25 23:25:19 +0800

    Added toggle feature for sending kafka messages

--- src/main/java/com/nus/iss/tasktracker/service/impl/KafkaProducerServiceImpl.java
+++ src/main/java/com/nus/iss/tasktracker/service/impl/KafkaProducerServiceImpl.java
@@ -1,7 +1,9 @@
 package com.nus.iss.tasktracker.service.impl;
 
 import com.nus.iss.tasktracker.service.KafkaProducerService;
+import com.nus.iss.tasktracker.util.TaskTrackerConstant;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 import lombok.extern.slf4j.Slf4j;
@@ -9,6 +11,10 @@ import lombok.extern.slf4j.Slf4j;
 @Service
 @Slf4j
 public class KafkaProducerServiceImpl implements KafkaProducerService {
+
+    @Value("${task-tracker.kafka.enabled:false")
+    private boolean kafkaEnabled;
+
     private final KafkaTemplate<String, String> kafkaTemplate;
 
     @Autowired
@@ -19,12 +25,16 @@ public class KafkaProducerServiceImpl implements KafkaProducerService {
     @Override
     public void sendMessage(String topic, String message) {
         try {
-            kafkaTemplate.send(topic,message);
-            System.out.println("Message sent successfully to topic " + topic);
+            if(kafkaEnabled){
+                kafkaTemplate.send(topic,message);
+                log.info("Message sent successfully to topic - {} ", topic);
+            } else{
+                log.info("Message not sent to topic - {} ", topic);
+            }
         } catch (Exception e ){
-            System.out.println("Failed to send message: " + e.getMessage());
+            log.error("Failed to send message to topic: {} ", topic);
             log.error(e.getMessage());
         }
-
     }
+    
 }
